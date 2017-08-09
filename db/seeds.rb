ef7c0917e528cfe599369bcef9075cb9390ec95b
Matthew Peterson <mbpeterson19@gmail.com>
2017-08-09 12:47:00 -0600

    deleted application controller pagination stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 
-Testimonial.create!([{
-	author: 'Ian Acker',
-	date: 'August 8, 2017',
-	subject: 'Currently Testing the Website',
-	body: 'Test: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fugit architecto suscipit animi, quo optio saepe pariatur quam qui quidem sequi obcaecati atque ullam fuga asperiores quod. Provident, id, quia.'
-	},
+Testimonial.create!([
 	{
 	author: 'Steph',
 	date: 'December 10, 2016',
